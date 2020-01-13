c4a35dd6ef3d12ae58ff314501e25c02dd679577
ruither.borba <delki8@gmail.com>
2020-01-13 08:43:22 -0300

    experimenting with service worker

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,26 @@
 self.addEventListener('install', (event) => {
     console.log('inside the install handler', event);
+    event.waitUntil(
+      caches.open('index.html').then(function(cache) {
+        return cache.addAll([
+          '/index.html',
+          '/offline.html',
+        ]);
+      })
+    );
 });
 
 self.addEventListener('activate', (event) => {
     console.log('inside the activate handler', event);
 });
 
-self.addEventListener(fetch, (event) => {
+self.addEventListener('fetch', (event) => {
     console.log('inside the fetch handler', event);
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+      }).catch(function() {
+          return caches.match('/offline.html');
+      })
+    )
 });
